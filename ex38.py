ee4ca0400a613bbac82c41f61839b77c959927fc
yuan_minglongze <129572345+yuan-minglongze@users.noreply.github.com>
2023-09-13 22:18:22 +0800

    39

--- ex38.py
+++ ex38.py
@@ -9,7 +9,7 @@ more_stuff = ["Day", "Night", "Song", "Firsbee",
 while len(stuff) != 10:
     next_one = more_stuff.pop()
     print("Adding: ", next_one)
-    stuff.append(next_one)
+    stuff.append(next_one) # type: ignore
     print(f"There are {len(stuff)} items now.")
 
 print("There we go: ", stuff)
